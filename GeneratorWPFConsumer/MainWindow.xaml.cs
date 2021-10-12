95c0a4781f9c19fd6c8543b0160a992a76183cf6
Peter Street <lazerfx@gmail.com>
2021-10-12 14:22:49 +0100

    Add project files.

---
+++ GeneratorWPFConsumer/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeneratorWPFConsumer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var dataSource = new TestClass(12.1m, 1.03m, 4.5m, 2.3m, "Test String");
+            InitializeComponent();
+            this.DataContext = dataSource;
+        }
+    }
+}
---
+++ GeneratorWPFConsumer/TestClass.cs
@@ -0,0 +1,27 @@
+﻿using AutoNotify;
+
+namespace GeneratorWPFConsumer
+{
+    public partial class TestClass
+    {
+        public TestClass(decimal Open, decimal Close, decimal High, decimal Low, string TestString)
+        {
+            this._open = Open;
+            this._close = Close;
+            this._high = High;
+            this._low = Low;
+            this._testString = TestString;
+        }
+
+        [AutoNotify]
+        private decimal _open;
+        [AutoNotify]
+        private decimal _close;
+        [AutoNotify]
+        private decimal _high;
+        [AutoNotify]
+        private decimal _low;
+        [AutoNotify]
+        private string _testString;
+    }
+}
